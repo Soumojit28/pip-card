e5f085ccd689d1f24da96fa8095d7a602dd2067c
Soumojit <soumojitash@gmail.com>
2021-10-22 00:24:20 +0530

    test: added rich library

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("PIP-CARD!")
-print("Testing!")
+from rich.console import Console
+from rich import box
+from rich.align import Align
+from rich.table import Table
+from rich.text import Text
+
+console = Console()
+
+table = Table(title="Hey Folks!", show_header=False)
+
+console.print("Hello", "World!", style="bold red", justify="center")
+
+
+
+# table.add_column(justify="center", style="cyan")
+# table.add_column(justify="left", style="magenta")
+
+
+# table_centered = Align.center(table)
+# # console.print(table_centered)
